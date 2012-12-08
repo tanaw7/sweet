deec661300ad6ed5997385c44001ce0ba3f3e12c
root <root@localhost.(none)>
2012-12-08 05:06:32 +0000

    s u g

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sweet::Application.config.secret_token = '9d7a65a83bd01b355b096bf945e13ae7f5198ce041ea27f393c4fab36346bed1bf122fdd34522adbd86cc3304ea727b54f87be235d8588fe5b12dd40f663dbc4'
